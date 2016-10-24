143f28da10cb6028a4adcb681fa130565acd76ff
Brijesh Singh <bsingh@saltside.se>
2016-10-24 17:48:50 +0530

    Circular Linked List Implementation

---
+++ src/main/java/com/ds/algo/linklist/SinglyCircularLinkedList.java
@@ -0,0 +1,34 @@
+package com.ds.algo.linklist;
+
+public class SinglyCircularLinkedList {
+
+	private SLNode head;
+	private SLNode tail;
+	private int size;
+	
+	public void add(int data){
+		SLNode node = new SLNode(data, null);
+		size++;
+		if(head==null){
+			head=node;
+			tail=head;
+			tail.next=head;
+		}else {
+			tail.next=node;
+			tail=node;
+			node.next=head;
+		}
+	}
+	public void display(){
+		SLNode current = head;
+		boolean arrow=false;
+		do {
+			System.out.print((arrow)?"--> |"+current.data+"|": "|"+current.data+"|");
+			arrow=true;
+			current=current.next;
+		} while (current!=head);
+	}
+	public void size(){
+		System.out.println(size);
+	}
+}
